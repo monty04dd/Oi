346471ea1bac6aed84e436f56416491c4aec93c0
monty04dd <luca.montanari.2004@icloud.com>
2024-11-20 16:20:51 +0100

    pipeline

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 import streamlit as st
-
+import joblib
+import pandas as pd
 def somma(l1:float, l2:float):
     a = l1 + l2
     return a
 
 def main():
-    st.text("Ciao questo front-end funziona")
-    #st.title('Luchino')
+    st.title("Inferenza per cars dataset")
+    st.text("""questa applicazione web permette id fare inferenza 
+sul dataset cars con un modello ridge""")
     # slider
     num1 = st.slider("inserisci lato1 rettangolo", 0, 100, 2)
     num2 = st.slider("inserisci lato2 rettangolo", 0, 100, 3)
+    option = st.selectbox('How would you like to be contacted?',('Email', 'Home phone', 'Mobile phone'))
     r = somma(num1, num2)
 
     st.write(f"la somma è: {r}")
