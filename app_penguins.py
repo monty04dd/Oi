95b2bdbf9450457f78cc246d637122cc04d6b1f7
monty04dd <luca.montanari.2004@icloud.com>
2024-11-20 21:00:44 +0100

    penguins

--- app_penguins.py
+++ app_penguins.py
@@ -5,12 +5,18 @@ from PIL import Image
 
 def main():
     loaded = joblib.load("pipeline_penguins.pkl")
-    st.markdown("""### tramite il modello 
-                RandomForestRegressoior ho creato la webapp""")
+    st.text("""### tramite il modello RandomForestRegressoior ho creato la webapp""")
     st.markdown("# CHE PINGUINO SEI?😎")
 
 
 
+    bill_length_mm = st.number_input("Inserisci la lunghezza del becco", value=None, placeholder="0.00")
+    bill_length_mm = st.number_input("Inserisci la profondità del becco", value=None, placeholder="0.00")
+    flipper_length_mm = st.slider("inserisci la lunghezza delle ali", 0, 232, 0)
+    body_mass_g = st.slider("inserisci il peso in grammi", 0, 6300, 0)
+    sex = st.selectbox("di che sesso è?", ("male","female"), key="Pclass")
+    island = st.selectbox("di che sesso è?", ("Torgersen","Biscoe","Dream"), key="Pclass")
+    
 
 if __name__ == "__main__":
     main()
