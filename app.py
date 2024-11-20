a125909025e880122a33fe420875727b2a683e27
monty04dd <luca.montanari.2004@icloud.com>
2024-11-20 11:44:42 +0100

    app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+def somma(l1:float, l2:float):
+    a = l1 + l2
+    return a
+
+def main():
+    st.text("Cia0 questo front-end funiona")
+    # slider
+    num1 = st.slider("inserisci lato1 rettangolo", 0, 100, 2)
+    num2 = st.slider("inserisci lato2 rettangolo", 0, 100, 3)
+    r = somma(num1, num2)
+
+    st.write(f"la somma è: {r}")
+
+if __name__ == "__main__":
+    main()
